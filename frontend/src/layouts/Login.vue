296cdc3ae0e9ee9d227a5df230fe94f691d7ca3d
JuneHyung <cjh951114@gmail.com>
2021-09-20 15:04:34 +0900

    Update Login : 로그인폼추가

--- frontend/src/layouts/Login.vue
+++ frontend/src/layouts/Login.vue
@@ -1,13 +1,13 @@
 <template>
-  <q-layout>
-    <p>HelloLogin</p>
-  </q-layout>
+  <LoginForm></LoginForm>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import LoginForm from "./../components/member/LoginForm";
 export default defineComponent({
   name: "Login",
+  components: { LoginForm },
 });
 </script>
 
