74f6620de12fcd56287ba5a46a7496e883f10ebd
JuneHyung <cjh951114@gmail.com>
2021-09-20 14:52:44 +0900

    Update Find : 지도영역 파일 및 목록테이블 틀 추가

---
+++ frontend/src/components/find/GMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>구글 지도영역입니다.</p>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "GMap",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style></style>
--- frontend/src/layouts/Find.vue
+++ frontend/src/layouts/Find.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-layout>
-    <p>HelloStore</p>
-  </q-layout>
+  <GMap></GMap>
+  <GMapList></GMapList>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import GMap from "./../components/find/GMap.vue";
+import GMapList from "./../components/find/GMapList.vue";
 export default defineComponent({
   name: "Find",
+  components: { GMap, GMapList },
 });
 </script>
 
